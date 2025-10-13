1f8af31421edf7e58b0e050e5d40572471fd7f84
NwekeFavour <buchijunior90@gmail.com>
2025-10-13 15:29:19 +0100

    changed the smtp

--- config/mailer.js
+++ config/mailer.js
@@ -4,15 +4,18 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail", // or use another provider like "hotmail", "yahoo"
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
-    user: process.env.EMAIL_USER, // your email address
-    pass: process.env.EMAIL_PASS, // your email app password
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
-    tls: {
-        rejectUnauthorized: false,
-    }
+  tls: {
+    rejectUnauthorized: false,
+  }
 });
+
 
 transporter.verify((error, success) => {
   if (error) {
